e3cc430472dd833e5cc086e6e25bd0a15b622f89
theone11show <team@theone11.show>
2025-09-10 13:21:38 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import Header from "../components/Header"
-import HeroSection from "../components/HeroSection";
-import AboutSection from "../components/AboutSection";
-import OurBrands from "../components/OurBrands";
-import OurMission from "../components/OurMission";
+import React from 'react'; 
+import Header from "../components/Header" 
+import HeroSection from "../components/HeroSection"; 
+import AboutSection from "../components/AboutSection"; 
+import OurBrands from "../components/OurBrands"; 
+import OurMission from "../components/OurMission"; 
 import OurGoals from "../components/OurGoals";
-import OurVision from "../components/OurVision"
+ import OurVision from "../components/OurVision"
+
 const Home = () => {
-	return (
-		<div className="overflow-x-hidden">
-		<Header/>
-		<HeroSection/>
-		<AboutSection/>
-		<OurGoals/>
-		<OurMission/>
-		<OurVision/>
-		<OurBrands/>
-		
-		</div>
-	)
-}
-
-export default Home
-
-
+  return (
+    <div className="overflow-x-hidden">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <OurGoals />
+      <OurMission />
+      <OurVision />
+      <OurBrands />
+    </div>
+  );
+};
+export default Home;
