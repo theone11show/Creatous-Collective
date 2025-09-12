5d48fa5c8d6aa81c4992822cc54c01ffb1ee7849
theone11show <team@theone11.show>
2025-09-12 19:29:25 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.png"; 
+import logo from "../assets/Headerlogo.png"; 
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -56,3 +56,4 @@ const Header = () => {
 };
 
 export default Header;
+
