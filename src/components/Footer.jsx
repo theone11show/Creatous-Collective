e952b72445b1fcb3a93985d47146a4b085d5886d
theone11show <team@theone11.show>
2025-09-13 11:43:32 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,63 +2,72 @@ import React from "react";
 import { SlSocialFacebook } from "react-icons/sl";
 import { SiInstagram } from "react-icons/si";
 import { BsTwitterX } from "react-icons/bs";
+import { RiLinkedinBoxLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <footer
-      className="relative w-full py-10 px-6 overflow-hidden"
+      className="relative w-full min-h-[500px] py-16 px-6 overflow-hidden"
       style={{
-        background: "linear-gradient(to bottom, #d2d2d2, #a9a9a9, #a9a9a9)", 
+        background: "linear-gradient(to bottom, #d2d2d2, #a9a9a9, #a9a9a9)",
       }}
     >
-      <div className="relative mx-auto text-center max-w-6xl">
+      <div className="relative mx-auto text-center ">
         {/* Title */}
         <div
-          className="mb-4 tracking-wide mt-8 text-[1.5rem] md:text-[28px] sm:text-[18px] font-[Glacial_Indifference]"
+          className="mb-6 tracking-wide mt-[60px] md:mt-[80px] text-[2rem] md:text-[30px] sm:text-[18px] font-[Glacial_Indifference]"
         >
           CREATOUS COLLECTIVE
         </div>
 
         {/* Divider */}
-        <div className="w-11/12 md:w-3/4 border-t-2 border-black/70 mx-auto mb-6"></div>
+        <div className="w-11/12 md:w-3/4 border-t-2 border-black/70 mx-auto mb-8"></div>
 
         {/* Contact + Social */}
-        <div className="flex flex-col md:flex-row items-center justify-between text-center md:text-left mb-6">
+        <div className="flex flex-col md:flex-row items-center justify-between text-center md:text-left">
           {/* Left */}
-          <div className="w-full md:w-1/3 flex justify-center md:justify-start mb-4 md:mb-0">
+          <div className="w-full md:w-1/3 flex justify-center md:justify-start mb-6 md:mb-0">
             <a
-              href="mailto:info@theone11.show"
-              className="text-black md:text-[20px] sm:text-[16px] font-[Montserrat]"
+              href="mailto:creatouscollective@gmail.com"
+              className="text-black md:text-[22px] sm:text-[18px] font-[Montserrat]"
             >
-              @creatouscollective
+              creatouscollective@gmail.com
             </a>
           </div>
 
           {/* Center - Social Media */}
-          <div className="w-full md:w-1/3 flex justify-center space-x-6 text-xl mb-4 md:mb-0">
+          <div className="w-full md:w-1/3 flex justify-center space-x-8 text-2xl mb-6 md:mb-0">
             <a
               href="https://www.facebook.com/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-red-500 transition duration-300"
+              className="hover:text-[#1877F2] transition duration-300"
             >
-              <SlSocialFacebook size={24} />
+              <SlSocialFacebook size={28} />
             </a>
             <a
               href="https://x.com/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-red-500 transition duration-300"
+              className="hover:text-black transition duration-300"
             >
-              <BsTwitterX size={24} />
+              <BsTwitterX size={28} />
             </a>
             <a
-              href="https://www.instagram.com/"
+              href="https://www.instagram.com/creatouscollectivemedia/?hl=en"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-red-500 transition duration-300"
+              className="hover:text-[#E4405F] transition duration-300"
             >
-              <SiInstagram size={24} />
+              <SiInstagram size={28} />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/creatous-collective-media/?originalSubdomain=in"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#0A66C2] transition duration-300"
+            >
+              <RiLinkedinBoxLine size={32} />
             </a>
           </div>
 
@@ -66,17 +75,17 @@ const Footer = () => {
           <div className="w-full md:w-1/3 flex justify-center md:justify-end">
             <a
               href="tel:+917477785294"
-              className="text-black md:text-[20px] sm:text-[16px] font-[Montserrat]"
+              className="text-black md:text-[22px] sm:text-[18px] font-[Montserrat]"
             >
-              @creatouscollective
+              +91 74777 85294
             </a>
           </div>
         </div>
 
         {/* Policy Links */}
         <div
-          className="mt-6 mb-4 flex flex-col md:flex-row justify-center items-center
-          space-y-2 md:space-y-0 md:space-x-6 text-black md:text-[18px] sm:text-[14px]"
+          className="mt-12 mb-4 flex flex-col md:flex-row justify-center items-center
+          space-y-2 md:space-y-0 md:space-x-8 mt-[60px] md:mt-[80px] text-black md:text-[22px] sm:text-[18px]"
           style={{
             fontFamily: "Montserrat, sans-serif",
           }}
@@ -86,7 +95,7 @@ const Footer = () => {
         </div>
 
         {/* Copyright */}
-        <p className="text-[11px] sm:text-xs md:text-sm font-[Montserrat]">
+        <p className="text-[12px] sm:text-xs md:text-sm font-[Montserrat]">
           &copy; 2025 Creatous Collective Private Limited. All rights reserved.
         </p>
       </div>
